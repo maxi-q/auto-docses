68832eb4fcba8812261b3e66d3216a7e20494fdc
maxi-q <maximkabust@gmail.com>
2024-02-23 17:42:00 +0300

    change api url

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,5 +1,8 @@
-// export const API_URL = 'http://26.81.229.58:8000/api/'
-// export const Server_URL = 'http://26.81.229.58:8000'
+// export const API_URL = 'http://192.168.1.29:8000/api/'
+// export const Server_URL = 'http://192.168.1.29:8000'
 
-export const API_URL = 'https://autodocs.online/api/'
-export const Server_URL = 'https://autodocs.online'
+export const Server_URL = 'http://backend:8000/'
+export const API_URL = Server_URL + 'api/'
+
+// export const API_URL = 'https://autodocs.online/api/'
+// export const Server_URL = 'https://autodocs.online'
