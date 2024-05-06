3b20114b236567054e3ee19489ffbe733f8c2250
maxi-q <maximkabust@gmail.com>
2024-05-06 10:05:23 +0300

    API

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,14 +1,14 @@
 // export const API_URL = 'http://192.168.1.29:8000/api/'
 // export const Server_URL = 'http://192.168.1.29:8000'
 
-export const Server_URL = 'http://192.168.28.174:8000/'
-export const API_URL = Server_URL + 'api/'
+// export const Server_URL = 'http://192.168.28.174:8000/'
+// export const API_URL = Server_URL + 'api/'
 
 // export const Server_URL = 'http://backend:8000/'
 // export const API_URL = Server_URL + 'api/'
 
-// export const Server_URL = 'http://localhost:8000/'
-// export const API_URL = Server_URL + 'api/'
+export const Server_URL = 'http://localhost:8000/'
+export const API_URL = Server_URL + 'api/'
 
 // export const API_URL = 'https://autodocs.online/api/'
 // export const Server_URL = 'https://autodocs.online'
